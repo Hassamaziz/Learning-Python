de80526f1d5ea8a9bbd32a12a3f9557af9fc2244
hassamaziz <hassamaziz001@gmail.com>
2024-05-07 16:45:29 +0500

    variables in python

---
+++ Variables And Coments in python/Main.py
@@ -0,0 +1,21 @@
+print("Hello World")
+print("Hello hassam")
+
+# Comments in Python
+# print("Hello World")
+# print("Hello World")
+# print("Hello World")
+# print("Hello World")
+# print("Hello World")
+'''
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+Multi line comment is done by triple single quotes
+'''
+# comments in python are done by hashTag sign(#)
+
+
+# Variables in Python
