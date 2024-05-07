175db8a4ec2c2fcf2ecf5093f51c8c208daca28d
hassamaziz <hassamaziz001@gmail.com>
2024-05-07 17:42:35 +0500

    Operators in python

---
+++ Operators/main.py
@@ -0,0 +1,45 @@
+num1 = 100
+num2= 22
+
+#Arithmetic Operators
+print("Addition : ",num1 + num2)
+print("Subtraction : ",num1 - num2)
+print("Multiplication : ",num1 * num2)
+division = num1 / num2
+print("Division : ", division)
+remainder = num1 % num2
+print("Remainder/Modulo : ", remainder)
+doubledivide = num1//num2
+print("Double Division (floor division): ", doubledivide)
+
+#Assignment operators
+a=4
+print(a)
+a +=  4 # add and assign
+print(a)
+a -=  4 # subtract and assign
+print(a)
+a *=  4 # multiply and assign
+print(a)
+a /=  4 # divide and assign
+print(a)
+
+
+#Comparison operators
+b=8
+c=3
+d=8
+print(b>c)
+print(b<c)
+print(b==c)
+print(b!=c)
+print(b<=c)
+print(b>=c)
+#Logical operators AND OR NOT
+# print(True and True)
+# print(False and False)
+# print(True or False)
+# print(not True)
+print(b>c and b==d)
+print(b>c or d<c)
+
