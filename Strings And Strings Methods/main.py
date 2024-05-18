f56a687c48e1e2dcbff29f96731b5dc3af24918a
hassamaziz <hassamaziz001@gmail.com>
2024-05-19 00:46:20 +0500

    Strings

---
+++ Strings And Strings Methods/main.py
@@ -0,0 +1,14 @@
+name = "hassam aziz"
+number = "7"
+print(name)
+
+# Now for Trimming or splicing
+# print(name[0:3])
+
+# Some methods
+
+print(name.upper())
+print(name.capitalize())
+print(name.count("h"))
+print(number.isnumeric())
+print(name.isdigit())
