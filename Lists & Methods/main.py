46a8e5a75b4637ea9f97e35809166fdc062149a6
hassamaziz <hassamaziz001@gmail.com>
2024-05-26 23:38:34 +0500

    Lists and methods

---
+++ Lists & Methods/main.py
@@ -0,0 +1,12 @@
+l1 = [1,2,3,4,5,6,7,8,9,10,"hassam","aziz"]
+print(type(l1))
+print(l1)
+l1.remove("aziz")
+print(l1)
+l2=[3434,34,356,2,456,1,45,65]
+print(l2)
+l2.sort()
+print(l2)
+l2.pop()
+l2.append(98)
+print(l2)
