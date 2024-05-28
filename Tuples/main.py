b3bef35ed9d365a7af393cd691a27ad0b6b532a9
hassamaziz <hassamaziz001@gmail.com>
2024-05-28 23:20:23 +0500

    .

--- Tuples/main.py
+++ Tuples/main.py
@@ -1,4 +1,4 @@
 t1=(2,3,4,5,5,5)
 print(t1)
 print(t1.count(5))
-# t1.pop() Tuple is immutable just like strings so this line will give an error
+# t1.pop() Tuple is immutable just like strings, so this line will give an error
