7514b510d2772a9582fe46ab009d949df17dd65a
hassamaziz <hassamaziz001@gmail.com>
2024-05-29 21:35:38 +0500

    dictionaries

--- dict & methods/main.py
+++ dict & methods/main.py
@@ -1,2 +1,8 @@
-dict1={}
-print(type(dict1))
+# dict1={}
+# print(type(dict1))
+
+dict2={"good":"Something pleasant","hello":"A formal greeting","1":"the number one"}
+print(dict2["hello"])
+
+marks={"Hassam":79,"Waleed":67,"Talha":56}
+print(marks["Hassam"])
