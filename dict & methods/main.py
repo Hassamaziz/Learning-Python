f66e4dffc9fd5f0eaadb14b4abe03b9dc708e5c6
hassamaziz <hassamaziz001@gmail.com>
2024-05-29 21:42:42 +0500

    Dictionaries and Methods

--- dict & methods/main.py
+++ dict & methods/main.py
@@ -6,3 +6,14 @@ print(dict2["hello"])
 
 marks={"Hassam":79,"Waleed":67,"Talha":56}
 print(marks["Hassam"])
+
+# Adding something in dictionaries
+marks["Saif"]=95
+print(marks)
+# .get method saves you from getting an error
+
+# print(marks.get("Hassam aziz")) # Output will be NONE
+
+print(marks.keys())
+print(marks.values())
+print(marks.items())
