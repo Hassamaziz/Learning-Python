c8aea3267c8c8a6d4e3d0cc6467090ad4f968d14
hassamaziz <hassamaziz001@gmail.com>
2024-06-26 14:38:10 +0500

    If Else_IF

--- If-else/main.py
+++ If-else/main.py
@@ -1,6 +1,15 @@
+# age = int(input("Enter Your Age: "))
+
+# if(age>=18):
+#     print("Yes.You can drive.")
+# else:
+#  print("No,you can go home.")
+
 age = int(input("Enter Your Age: "))
 
 if(age>=18):
     print("Yes.You can drive.")
+elif(age<12):
+   print("you are a kid.")
 else:
  print("No,you can go home.")
