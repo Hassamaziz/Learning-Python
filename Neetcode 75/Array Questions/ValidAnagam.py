fb98d60221288d4eb6a1a284c4114a0c87ce84e5
hassamaziz <hassamaziz001@gmail.com>
2025-08-12 21:49:33 +0500

    Add isAnagram function to check if two strings are anagrams

---
+++ Neetcode 75/Array Questions/ValidAnagam.py
@@ -0,0 +1,7 @@
+# Given two strings s and t, return true if t is an anagram of s and false otherwise.
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        return sorted(s) == sorted(t)
