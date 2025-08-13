1732d899643ca0afc1e85bdd0394a6d95affd344
hassamaziz <hassamaziz001@gmail.com>
2025-08-13 20:02:59 +0500

    Group Anagrams

---
+++ Neetcode 75/Array Questions/GroupAnagrams.py
@@ -0,0 +1,14 @@
+# Group Anagrams
+# Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.
+
+# An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        anagram_dict = {}
+        for s in strs:
+            sorted_str = ''.join(sorted(s))
+            if sorted_str not in anagram_dict:
+                anagram_dict[sorted_str] = []
+            anagram_dict[sorted_str].append(s)
+        return list(anagram_dict.values())
